eceb5a2e6d5d990b0036c8e32c625ea66a3d41a2
stayam kumar <s23singh2000@gmail.com>
2022-05-01 08:05:09 +0530

    added

---
+++ components/AppBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app-bar
+    :clipped-left="clipped"
+    fixed
+    dense
+    color="purple"
+    app
+  >
+    <v-toolbar-title class="mr-5 white--text">
+      {{ title }}
+    </v-toolbar-title>
+    <span class="white--text">SQL Editor challenge</span>
+    <v-spacer/>
+    <v-btn
+      icon
+      x-large
+      :link="true"
+      href="https://github.com/satyamsingh-sg/atlan-forntend-assignment-sql-query.git"
+    >
+      <v-icon>mdi-github</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+  data () {
+    return {
+      clipped: true,
+      title: 'Atlan',
+      mini: false,
+    }
+  },
+}
+</script>
---
+++ components/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-banner
+    v-if="showResult"
+    single-line
+    sticky
+  >
+    <span class="purple--text mr-3 caption">success</span>
+    <span class="white--text caption">({{ selectedQuery.queryResult.length }} rows)0.189 seconds</span>
+    <v-spacer/>
+  </v-banner>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: "Banner",
+  computed: {
+    ...mapState([
+      'selectedQuery',
+      'showResult'
+    ])
+  },
+}
+</script>
